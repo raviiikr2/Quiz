1d6a4ab3fa041f4778896c20cfb0dd2abc110e7a
raviiikr02 <ravikumarbit12312008@iiitsonepat.ac.in>
2025-11-01 17:20:32 +0530

    Finished the home screen work

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -5,44 +5,47 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(context) {
-    return Container(
-      height: double.infinity,
-      width: double.infinity,
-      decoration: const BoxDecoration(
-        image: DecorationImage(image: 
-        AssetImage('assets/images/quiz-logo.png')),
-        gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 127, 82, 211),
-            Color.fromARGB(255, 127, 82, 211),
-          ],
-        ),
-      ),
+    return Center(
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
         children: [
-          Text(
-            'Learn Flutter the fun Way!',
-            style: TextStyle(
-              fontSize: 24,
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
+          Image.asset(
+            'assets/images/quiz-logo.png',
+
+            //without the help of opacity
+            color: const Color.fromARGB(150, 255, 255, 255),
+            width: 200,
+          ),
+
+          //with the help of opacity
+          //   Opacity(
+          //     opacity: 0.5,
+          //  child:  Image.asset('assets/images/quiz-logo.png',
+          //   width: 200),
+          //   ),
+          const SizedBox(height: 40),
+          const Text(
+            "Learn The Flutter In Fun Way!!",
+            style: TextStyle(color: Colors.white, fontSize: 25),
+          ),
+          const SizedBox(height: 40),
+
+          // this outlinedButton.icon help to add icons in the button
+          OutlinedButton.icon(
+            onPressed: () {},
+
+            //styling the button
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+              backgroundColor: const Color.fromARGB(255, 139, 28, 231),
+              textStyle: TextStyle(fontSize: 20),
+            ),
+            icon: const Icon(Icons.arrow_right_alt, size: 40),
+            label: const Text("Start Quiz",
+            style: TextStyle(fontSize: 22,fontWeight: FontWeight.bold),
             ),
           ),
-          const SizedBox(height: 20),
-          TextButton(
-            onPressed: () {},
-            style: TextButton.styleFrom(
-              backgroundColor: const Color.fromARGB(255, 3, 0, 1),
-              foregroundColor: Colors.white,
-              elevation: 100,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadiusGeometry.all(Radius.zero),
-              ),
-            ),
-            child: const Text('Start Quiz',
-            style: TextStyle(fontSize: 20),),
-          ),],
+        ],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,11 @@ import 'package:adv_app/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Scaffold(body: HomeScreen())));
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Color.fromARGB(244, 133, 3, 240),
+      body: HomeScreen()
+      )
+      )
+      );
 }
